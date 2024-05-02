57015dc0132be2c36b5064d917b130b7b2998c71
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-05-02 12:17:22 +0900

    💄 create basic page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Card, Typography } from '@mui/material';
+import SimpleAppBar from './component/SimpleAppBar';
+
 function App() {
-    return (
-        <>
-            <h1>Vite + React</h1>
-            <div className="card"></div>
-        </>
-    );
+  return (
+    <>
+      <SimpleAppBar />
+      <Card sx={{ m: 2, p: 2 }}>
+        <Typography variant="h4" component="div" gutterBottom>
+          1. Hello World!
+        </Typography>
+      </Card>
+      <Card sx={{ m: 2, p: 2 }}>
+        <Typography variant="h4" component="div" gutterBottom>
+          2. Hello World!
+        </Typography>
+      </Card>
+    </>
+  );
 }
 
 export default App;
